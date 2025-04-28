c470e0d901ad176060f851b9b6da53dcfdc78624
Ismael <ismael.tosun@gmail.com>
2025-04-28 13:12:30 -0400

    Added main page, icon, settings modal

---
+++ .idea/Main.js
@@ -0,0 +1,8 @@
+function settings() {
+    document.getElementsByClassName("settings")[0].style.display = "block";
+}
+
+function saveAndClose() {
+    document.getElementsByClassName("settings")[0].style.display = "none";
+}
+
